40565a2f05991aff2a2231358f1202d1c72dd886
chicori3 <chicori3@naver.com>
2021-02-09 20:23:22 +0900

    Router Setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  // React 환경변수 : REACT_APP_~~
+  apiKey: process.env.REACT_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+export default firebase.initializeApp(firebaseConfig);
