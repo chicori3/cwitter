85bee45a2caf93956ccf843217202f5974bf256f
chicori3 <chicori3@naver.com>
2021-02-17 22:55:28 +0900

    Firebase Log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { authService } from "myBase";
 import AppRouter from "components/Router";
 
 function App() {
-  // 로그인 유저 확인
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // firebase 초기화
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Loading..."}</>;
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -26,9 +27,13 @@ const Auth = () => {
         await authService.signInWithEmailAndPassword(email, password);
       }
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+  const toggleAccount = () => {
+    setNewAccount((prev) => !prev);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -48,8 +53,15 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Join" : "Login"} />
+        {error}
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign in"}
+        />
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
