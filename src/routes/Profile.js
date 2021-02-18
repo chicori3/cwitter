9240e946fa65260407ac25b60615864ab89197ad
chicori3 <chicori3@naver.com>
2021-02-19 00:16:50 +0900

    Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { authService } from "myBase";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  // 로그아웃 Hooks
+  const history = useHistory();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
