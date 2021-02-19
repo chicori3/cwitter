fbe675c96ab57a140e98259b8ba308d51c8136bd
chicori3 <chicori3@naver.com>
2021-02-19 17:10:38 +0900

    Database Setup

--- src/myBase.js
+++ src/myBase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const firebaseConfig = {
   // React 환경변수 : REACT_APP_~~
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [cweet, setCweet] = useState("");
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setCweet(value);
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          value={cweet}
+          onChange={onChange}
+          type="text"
+          placeholder="뭐라도 써주실래요?"
+          maxLength={120}
+        />
+        <input type="submit" value="Cweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
