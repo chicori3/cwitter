237551e9278bcae24eb2a72120428a488fec507c
chicori3 <chicori3@naver.com>
2021-03-02 23:36:16 +0900

    Get Image and Clear

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Cweet from "components/Cweet";
 const Home = ({ userObj }) => {
   const [cweet, setCweet] = useState("");
   const [cweets, setCweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     dbService
@@ -46,7 +47,18 @@ const Home = ({ userObj }) => {
     const theFile = files[0];
 
     const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+
+      setAttachment(result);
+    };
     reader.readAsDataURL(theFile);
+  };
+
+  const onClearFile = () => {
+    setAttachment(null);
   };
 
   return (
@@ -61,6 +73,12 @@ const Home = ({ userObj }) => {
         />
         <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Cweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="img" width="50px" height="50px" />
+            <button onClick={onClearFile}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {cweets.map((cweet) => (
