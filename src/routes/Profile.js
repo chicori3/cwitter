402bcd612190d289c9ce57f76ce99f71ad3af2b9
chicori3 <chicori3@naver.com>
2021-03-06 00:09:45 +0900

    Cweet Filtering

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { authService } from "myBase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "myBase";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   // 로그아웃 Hooks
   const history = useHistory();
 
@@ -10,6 +10,22 @@ const Profile = () => {
     authService.signOut();
     history.push("/");
   };
+
+  // 필터링
+  const getMyCweets = async () => {
+    const cweets = await dbService
+      .collection("cweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+
+    console.log(cweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyCweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
